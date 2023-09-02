3060e9128c20e10da98c4bb3dd983f1bb694e2c2
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-09-02 15:13:38 +0200

    Create table "Comments"
    - Added model for comment
    - Added migration file

--- src/database/sql/alchemy_models.py
+++ src/database/sql/alchemy_models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID, SQLAlchemyBaseOAuthAccountTableUUID
 from sqlalchemy import String, Integer, DateTime, func
 from sqlalchemy.orm import DeclarativeBase, relationship, Mapped, mapped_column
@@ -23,3 +25,16 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
 #     __tablename__ = "tags"
 #     id: Mapped[int] = mapped_column(Integer, primary_key=True)
 #     name: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
+
+
+#####___________COMMENTS MODELS____START___#################
+class Comment(Base):
+    __tablename__ = "comments"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    picture_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    comment_text: Mapped[str] = mapped_column(String(150))
+    created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now(), nullable=True)
+    updated_at: Mapped[date] = mapped_column('updated_at', DateTime, default=func.now(), onupdate=func.now(),
+                                             nullable=True)
+#####___________COMMENTS MODELS____END___#################
