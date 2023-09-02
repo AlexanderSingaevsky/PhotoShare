8793e3a335b01a02718781c05c69ded7d2bd0f29
Andrii <123271442+DarthRomanov@users.noreply.github.com>
2023-09-02 10:48:43 +0300

    Add files via upload

--- src/tag/routes.py
+++ src/tag/routes.py
@@ -1,4 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.database.sql.postgres_conn import database
+from src.database.cache.redis_conn import cache_database
+from src.tag.schemas import TagCreate, TagUpdate, Tag
+from src.tag.repository import TagRepository
+from src.image.repository import ImageRepository
 
 
 async def get_posts_by_tag(tag_name: str):
@@ -7,8 +13,8 @@ async def get_posts_by_tag(tag_name: str):
     for post in ImageRepository:
         if tag_name in post.tags:
             matching_posts.append(post)
-    
+
     if not matching_posts:
         raise HTTPException(status_code=404, detail=f"Пости з тегом '{tag_name}' не знайдені")
-    
+
     return matching_posts
