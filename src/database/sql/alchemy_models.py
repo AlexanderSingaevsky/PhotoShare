e8e52e65f286e24a226d7dd19166761f38073282
NightSpring1 <deroy193@gmail.com>
2023-09-02 15:19:22 +0300

    alembic fix, minor refactor

--- src/database/sql/alchemy_models.py
+++ src/database/sql/alchemy_models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Integer, String, Column
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID, SQLAlchemyBaseOAuthAccountTableUUID
+from sqlalchemy import String, Integer, DateTime, func
+from sqlalchemy.orm import DeclarativeBase, relationship, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs
 
 
@@ -7,19 +8,18 @@ class Base(AsyncAttrs, DeclarativeBase):
     pass
 
 
-class User(Base):
-    __tablename__ = "users"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(String(50))
+class OAuthAccount(SQLAlchemyBaseOAuthAccountTableUUID, Base):
+    pass
 
 
-
-# задаємо класс тегів
-
-
-class Tag(Base):
-    """Represents a tag associated with posts."""
-    __tablename__ = "tags"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True, nullable=False)
-    
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    username: Mapped[str] = mapped_column(String(50), nullable=False)
+    access_level: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    created_at: Mapped[DateTime] = mapped_column('crated_at', DateTime, default=func.now())
+    oauth_accounts: Mapped[list[OAuthAccount]] = relationship("OAuthAccount", lazy="joined")
+      
+      
+# class Tag(Base):
+#     __tablename__ = "tags"
+#     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+#     name: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
--- src/tag/routes.py
+++ src/tag/routes.py
@@ -1,10 +1,4 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.ext.asyncio import AsyncSession
-from src.database.sql.postgres_conn import database
-from src.database.cache.redis_conn import cache_database
-from src.tag.schemas import TagCreate, TagUpdate, Tag
-from src.tag.repository import TagRepository
-from src.image.repository import ImageRepository
 
 
 async def get_posts_by_tag(tag_name: str):
