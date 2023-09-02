edfb67138b675db33b12372ff575dd9ff4c73dd5
rattlingmars8 <rattlingmars8@gmail.com>
2023-09-02 18:32:54 +0200

    reset password pt2 (new route)

--- src/auth/service.py
+++ src/auth/service.py
@@ -37,6 +37,7 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
 
         print(f"User {user.id} has forgot their password. Reset token: {token}")
 
+
     async def on_after_request_verify(
             self, user: User, token: str, request: Optional[Request] = None
     ):
