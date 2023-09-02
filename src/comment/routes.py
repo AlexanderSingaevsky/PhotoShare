9a7fa586ab77921410c85a0e1c260c4d9a5bef1e
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-09-02 17:08:33 +0200

    Added routes for comments

--- src/comment/routes.py
+++ src/comment/routes.py
@@ -1,1 +1,43 @@
-# comment routes
+from typing import List
+
+from fastapi import APIRouter, Path, Depends, status, Query
+from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.auth.schemas import UserRead, UserCreate, UserUpdate
+from src.auth.service import auth_backend, fastapi_users, google_oauth_client, SECRET
+from ..comment import repository as comments_repo
+from src.comment.schemas import CommentResponseSchema, CommentSchema, CommentUpdateSchema, CommentUpdateResponseSchema
+from src.database.sql.postgres_conn import database
+
+router = APIRouter(prefix="/comment", tags=["comments"])
+
+
+@router.get('/', name='Get all comments', response_model=List[CommentResponseSchema])
+async def get_all_comments(limit: int = Query(default=10, le=200), db: AsyncSession = Depends(database)):
+    comments = await comments_repo.get_all_comments(db, limit)
+    return comments
+
+
+@router.get('/{comment_id}', name='Get one comment', response_model=CommentResponseSchema)
+async def get_one_comment(comment_id: int = Path(ge=1), db: AsyncSession = Depends(database)):
+    comment = await comments_repo.get_one_comment(comment_id, db)
+    return comment
+
+
+@router.post('/', name="Create new comment", response_model=CommentResponseSchema, status_code=status.HTTP_201_CREATED)
+async def create_comment(body: CommentSchema, db: AsyncSession = Depends(database)):
+    comment = await comments_repo.create_new_comment(body, db)
+    return comment
+
+
+@router.put('/', name="Update existing comment", response_model=CommentUpdateResponseSchema,
+            status_code=status.HTTP_200_OK)
+async def update_comment(body: CommentUpdateSchema, db: AsyncSession = Depends(database)):
+    comment = await comments_repo.update_comment(body, db)
+    return comment
+
+
+@router.delete('/{comment_id}', name="Delete existing comment", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_comment(comment_id: int = Path(ge=1), db: AsyncSession = Depends(database)):
+    await comments_repo.remove_comment(comment_id, db)
+    return JSONResponse(content={"detail": f"Comment {comment_id} deleted!"})
