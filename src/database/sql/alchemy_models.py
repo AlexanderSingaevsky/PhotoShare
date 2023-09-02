0408cf43bac9764ccb3429e1f9340ac14f44e15a
Andrii <123271442+DarthRomanov@users.noreply.github.com>
2023-09-02 10:11:38 +0300

    Add files via upload

--- src/database/sql/alchemy_models.py
+++ src/database/sql/alchemy_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, Column
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs
 
@@ -11,3 +11,15 @@ class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String(50))
+
+
+
+# задаємо класс тегів
+
+
+class Tag(Base):
+    """Represents a tag associated with posts."""
+    __tablename__ = "tags"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True, nullable=False)
+    
