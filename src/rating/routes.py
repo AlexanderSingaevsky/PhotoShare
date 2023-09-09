dd2cef79ea6097d89ac31f02a6e67c5e892ad582
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-09-09 18:12:57 +0200

    Update routes.py

--- src/rating/routes.py
+++ src/rating/routes.py
@@ -1,20 +1,33 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, status, HTTPException, Path
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth.service import current_active_user
 from src.database.sql.alchemy_models import User
 from src.database.sql.postgres_conn import database
+from src.image.repository import ImageQuery
 from src.rating.repository import RatingQuery
 from src.rating.schemas import RatingSchemaResponse, RatingSchemaRequest, RatingUpdateSchemaRequest
 
 router = APIRouter(prefix="/rating", tags=["ratings"])
 
 
+@router.get('/{rating_id}', response_model=RatingSchemaResponse)
+async def get_comment(rating_id: int = Path(ge=1), user: User = Depends(current_active_user),
+                      db: AsyncSession = Depends(database)):
+    rating = await RatingQuery.read(rating_id, db)
+    if not rating:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Rating not found!')
+    return rating
+
+
 @router.post('/create', name="Add new rating", response_model=RatingSchemaResponse, status_code=status.HTTP_201_CREATED)
 async def create_rating(body: RatingSchemaRequest,
                         user: User = Depends(current_active_user),
                         db: AsyncSession = Depends(database)):
-    rating = await RatingQuery.create(body, user, db)
+    image = await ImageQuery.read(body.image_id, db)
+    if not image:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Image does not exist!')
+    rating = await RatingQuery.create(body, user, image, db)
     if not rating:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Image not found!')
     return rating
@@ -25,7 +38,17 @@ async def update_rating(rating_id: int,
                         body: RatingUpdateSchemaRequest,
                         user: User = Depends(current_active_user),
                         db: AsyncSession = Depends(database)):
-    rating = await RatingQuery.update(rating_id, body, db)
+    rating = await RatingQuery.read(rating_id, db)
     if not rating:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Rating not found!')
-    return rating
+    return await RatingQuery.update(rating_id, body, user, db)
+
+
+@router.delete('/delete/{rating_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_rating(rating_id: int,
+                        user: User = Depends(current_active_user),
+                        db: AsyncSession = Depends(database)):
+    rating = await RatingQuery.read(rating_id, db)
+    if not rating:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Rating not found!')
+    await RatingQuery.delete(rating_id, user, db)
