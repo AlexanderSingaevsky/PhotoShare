420eeed50fc236c85e2c9bee066e976f214c9f2b
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-09-09 18:12:51 +0200

    Update repository.py

--- src/rating/repository.py
+++ src/rating/repository.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -15,11 +16,20 @@ class RatingQuery:
         await db.commit()
 
     @staticmethod
-    async def create(body, user: User, db: AsyncSession) -> Rating:
-        image = await db.get(Image, body.image_id)
-        if not image:
-            return None
-        rating = Rating(**body.model_dump(), owner_id=user.id)
+    async def read(rating_id: int, db: AsyncSession) -> Rating | None:
+        sq = select(Rating).filter_by(id=rating_id)
+        rating = await db.execute(sq)
+        rating = rating.scalar_one_or_none()
+        return rating
+
+    @staticmethod
+    async def create(body, user: User, image: Image, db: AsyncSession) -> Rating:
+        rating = await db.scalar(
+            select(Rating).where((Rating.owner_id == user.id) & (Rating.image_id == image.id)))
+        if rating:
+            rating.value = body.value
+        else:
+            rating = Rating(**body.model_dump(), owner_id=user.id)
         db.add(rating)
         await db.commit()
         await db.refresh(rating)
@@ -27,23 +37,24 @@ class RatingQuery:
         return rating
 
     @staticmethod
-    async def update(rating_id, body, db) -> Rating | None:
-        sq = select(Rating).filter(Rating.id == rating_id)
-        result = await db.execute(sq)
-        rating = result.scalar_one_or_none()
-        if rating:
-            rating.value = body.value
-            await db.commit()
-            await db.refresh(rating)
-            image = await db.get(Image, rating.image_id)
-            await RatingQuery._update_average_rating(image, db)
+    async def update(rating_id: int, body, user, db) -> Rating | None:
+        rating = await db.scalar(
+            select(Rating).where((Rating.owner_id == user.id) & (Rating.id == rating_id)))
+        if not rating:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='This is not your rating!')
+        rating.value = body.value
+        await db.commit()
+        await db.refresh(rating)
+        image = await db.get(Image, rating.image_id)
+        await RatingQuery._update_average_rating(image, db)
         return rating
 
-    # @staticmethod #TODO
-    # async def delete(rating_id: int, db: AsyncSession) -> None:
-    #     sq = select(Rating).filter_by(id=rating_id)
-    #     result = await db.execute(sq)
-    #     rating = result.scalar_one_or_none()
-    #     if comment:
-    #         await db.delete(rating)
-    #         await db.commit()
+    @staticmethod
+    async def delete(rating_id: int, user, db: AsyncSession):
+        rating = await db.scalar(
+            select(Rating).where((Rating.owner_id == user.id) & (Rating.id == rating_id)))
+        if not rating:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='This is not your rating!')
+        await db.delete(rating)
+        await db.commit()
+        return rating
