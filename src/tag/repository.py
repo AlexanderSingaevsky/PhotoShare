f6ef542ec78396027b501743ec550edcb6b99264
Andrii <123271442+DarthRomanov@users.noreply.github.com>
2023-09-11 21:01:18 +0300

    Add files via upload

--- src/tag/repository.py
+++ src/tag/repository.py
@@ -4,12 +4,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.database.sql.alchemy_models import Tag, Image, ImageTag
 from src.tag.schemas import TagSchemaRequest
 
-
 class TagRepository:
     @staticmethod
-    async def create(
-        image: Image, tag_schema: TagSchemaRequest, session: AsyncSession
-    ) -> Image:
+    async def create(image: Image, tag_schema: TagSchemaRequest, session: AsyncSession) -> Image:
         for tag in tag_schema.names:
             stmt = select(Tag).where(Tag.name == tag)
             tag_to_append = await session.execute(stmt)
@@ -24,12 +21,21 @@ class TagRepository:
         return image
 
     @staticmethod
-    async def delete(
-        image: Image, tag_schema: TagSchemaRequest, session: AsyncSession
-    ) -> Image:
+    async def delete(image: Image, tag_schema: TagSchemaRequest, session: AsyncSession) -> Image:
         for tag in image.tags:
             if tag.name in tag_schema.names:
                 image.tags.remove(tag)
         session.add(image)
         await session.commit()
         return image
+
+    @staticmethod
+    async def search_images_by_tags(tag_names: list[str], session: AsyncSession) -> list[Image]:
+        stmt = (
+            select(Image)
+            .join(ImageTag, ImageTag.image_id == Image.id)
+            .join(Tag, Tag.id == ImageTag.tag_id)
+            .where(Tag.name.in_(tag_names))
+        )
+        images = await session.execute(stmt)
+        return images.scalars().all()
--- src/tag/routes.py
+++ src/tag/routes.py
@@ -7,40 +7,38 @@ from src.tag.schemas import TagSchemaRequest
 from src.tag.repository import TagRepository
 from src.image.repository import ImageQuery
 from src.auth.utils.access import access_service
+from src.database.sql.alchemy_models import  Image
 
-router = APIRouter(prefix="/tag", tags=["tags"])
+router = APIRouter(prefix='/tag', tags=["tags"])
 
 
 @router.post("/create", status_code=status.HTTP_201_CREATED)
-async def create_tag(
-    tag_data: TagSchemaRequest,
-    user: User = Depends(current_active_user),
-    session: AsyncSession = Depends(database),
-):
+async def create_tag(tag_data: TagSchemaRequest, user: User = Depends(current_active_user), session: AsyncSession = Depends(database)):
     image = await ImageQuery.read(tag_data.image_id, session)
     if not image:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Image does not exist!"
-        )
-    access = access_service("can_add_tag", user, image)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Image does not exist!')
+    access = access_service('can_add_tag', user, image)
     if not access.is_authorized:
         raise HTTPException(status_code=access.status_code, detail=access.detail)
     await TagRepository.create(image, tag_data, session)
-    return {"detail": "tags added"}
+    return {'detail': 'tags added'}
 
 
 @router.delete("/delete", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_tags(
-    tag_data: TagSchemaRequest,
-    user: User = Depends(current_active_user),
-    session: AsyncSession = Depends(database),
-):
+async def delete_tags(tag_data: TagSchemaRequest, user: User = Depends(current_active_user), session: AsyncSession = Depends(database)):
     image = await ImageQuery.read(tag_data.image_id, session)
     if not image:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Image does not exist!"
-        )
-    access = access_service("can_delete_tag", user, image)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Image does not exist!')
+    access = access_service('can_delete_tag', user, image)
     if not access.is_authorized:
         raise HTTPException(status_code=access.status_code, detail=access.detail)
     await TagRepository.delete(image, tag_data, session)
+
+@router.get("/search")
+async def search_images_by_tags(tag_name: str, session: AsyncSession = Depends(database)):
+    tags = await TagRepository.search_tags(tag_name, session)
+    if not tags:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Tag not found')
+    image_ids = [tag.image_id for tag in tags]
+    images = await ImageQuery.search_images_by_tags(image_ids, session)
+    return images
