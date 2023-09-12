2013817d5a79c1795e3d41609130afd315ad69db
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-09-13 00:41:32 +0200

    Documentation for rating and refactoring

--- src/rating/routes.py
+++ src/rating/routes.py
@@ -1,3 +1,16 @@
+"""
+Rating Routes
+
+This module defines the API routes for managing ratings.
+
+Routes:
+- GET /rating/{rating_id}: Delete a rating by its ID.
+- POST /rating/create: Create a new rating.
+- PUT /rating/update/{rating_id}: Update an existing rating.
+- DELETE /rating/delete/{rating_id}: Delete a rating.
+
+Each route expects specific parameters and returns HTTP status codes.
+"""
 from fastapi import APIRouter, Depends, status, HTTPException, Path
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -17,10 +30,19 @@ router = APIRouter(prefix="/rating", tags=["ratings"])
 
 @router.get("/{rating_id}", response_model=RatingSchemaResponse, name="Get one rating")
 async def get_rating(
-    rating_id: int = Path(ge=1),
-    user: User = Depends(current_active_user),
-    db: AsyncSession = Depends(database),
+        rating_id: int = Path(ge=1),
+        user: User = Depends(current_active_user),
+        db: AsyncSession = Depends(database),
 ):
+    """
+    Get a rating by its ID.
+
+    :param rating_id: int: The ID of the rating to retrieve.
+    :param user: User: The current user obtained from authentication.
+    :param db: AsyncSession: The database session.
+
+    :return: RatingSchemaResponse: The rating object.
+    """
     rating = await RatingQuery.read(rating_id, db)
     if not rating:
         raise HTTPException(
@@ -36,20 +58,22 @@ async def get_rating(
     status_code=status.HTTP_201_CREATED,
 )
 async def create_rating(
-    body: RatingSchemaRequest,
-    user: User = Depends(current_active_user),
-    db: AsyncSession = Depends(database),
+        body: RatingSchemaRequest,
+        user: User = Depends(current_active_user),
+        db: AsyncSession = Depends(database),
 ):
+    """
+    Create a new rating for an image. If the user has already rated the image,
+    update their previous rating.
+
+    :param body: RatingSchemaRequest: The rating data to create.
+    :param user: User: The current user obtained from authentication.
+    :param db: AsyncSession: The database session.
+
+    :return: RatingSchemaResponse: The created or updated rating object.
+    """
     image = await get_image(body.image_id, user, db)
-    if not image:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Image does not exist!"
-        )
     rating = await RatingQuery.create(body, user, image, db)
-    if not rating:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Image not found!"
-        )
     return rating
 
 
@@ -57,11 +81,22 @@ async def create_rating(
     "/update/{rating_id}", response_model=RatingSchemaResponse, name="Update rating"
 )
 async def update_rating(
-    rating_id: int,
-    body: RatingUpdateSchemaRequest,
-    user: User = Depends(current_active_user),
-    db: AsyncSession = Depends(database),
+        rating_id: int,
+        body: RatingUpdateSchemaRequest,
+        user: User = Depends(current_active_user),
+        db: AsyncSession = Depends(database),
 ):
+    """
+    Update a rating by its ID.
+
+    :param rating_id: int: The ID of the rating to update.
+    :param body: RatingUpdateSchemaRequest: The updated rating data.
+    :param user: User: Get the current user from authentication
+    :param db: AsyncSession: The database session.
+
+    :return: RatingSchemaResponse: The updated rating object.
+    """
+
     rating = await RatingQuery.read(rating_id, db)
     if not rating:
         raise HTTPException(
@@ -79,10 +114,19 @@ async def update_rating(
     "/delete/{rating_id}", status_code=status.HTTP_204_NO_CONTENT, name="Delete rating"
 )
 async def delete_rating(
-    rating_id: int,
-    user: User = Depends(current_active_user),
-    db: AsyncSession = Depends(database),
+        rating_id: int,
+        user: User = Depends(current_active_user),
+        db: AsyncSession = Depends(database),
 ):
+    """
+        Delete a rating by its ID.
+
+        :param rating_id: int: The ID of the rating to delete.
+        :param user: User: The current user obtained from authentication.
+        :param db: AsyncSession: The database session.
+
+        :return: None
+        """
     rating = await RatingQuery.read(rating_id, db)
     if not rating:
         raise HTTPException(
